4103e6099233680a62c30369f540bdff29a934c2
DTure <116016730+DTure@users.noreply.github.com>
2023-04-22 22:43:56 +0300

    Кря

---
+++ Counter.java
@@ -0,0 +1,39 @@
+package transaction;
+
+import java.awt.Component;
+
+import javax.swing.JTextField;
+
+public class Counter implements IfromTo{
+	private int count;
+	private JTextField textField;
+	
+	public Counter(JTextField textField) {
+		this.textField = textField;
+		this.count = 0;
+	}
+
+	@Override
+	public void onOut(Student st) {	
+	}
+
+	@Override
+	public void onIn(Student st) {	
+	}
+
+	@Override
+	public Component getComponent() {	
+		return textField;
+	}
+	
+	public void setCount(int count) {
+		this.count = count;
+		textField.setText(String.valueOf(count));
+	}
+
+	public int getCount() {
+		this.count = Integer.parseInt(textField.getText());
+		return count;
+	}
+	
+}
