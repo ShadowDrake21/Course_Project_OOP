8e04141782fb737ea42631323951c54e12cfeef6
Dmytro <122775021+ShadowDrake21@users.noreply.github.com>
2023-04-23 13:48:10 +0300

    !!!!

--- Krapyvianskyi_CourseProject/src/transaction/Door.java
+++ Krapyvianskyi_CourseProject/src/transaction/Door.java
@@ -1,5 +1,48 @@
 package transaction;
 
-public class Door {
+import java.awt.Component;
 
+import javax.swing.JLabel;
+
+import view.VisualPart;
+
+public class Door implements IfromTo, Runnable{
+	private VisualPart gui;
+	private int speed = 3;
+	private Student student;
+	private Thread thread;
+	private JLabel label;
+	
+	public Door(VisualPart gui, JLabel label) {
+		this.gui = gui;
+		this.label = label;
+	}
+	
+	@Override
+	public void run() {
+		synchronized (gui) {
+			do {
+				student = new Student(this.gui, this, 3);
+				(this.thread = new Thread(this.student)).start();
+				try {
+					Thread.sleep(this.speed * 1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			} while(!this.gui.getEndState());
+		}
+		
+	}
+
+	@Override
+	public void onOut(Student tr) {}
+
+	@Override
+	public void onIn(Student tr) {}
+
+	@Override
+	public Component getComponent() {
+		return label;
+	}
+	
 }
