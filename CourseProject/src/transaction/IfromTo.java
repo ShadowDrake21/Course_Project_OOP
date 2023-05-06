e2723daf77aab9e154c7eb02a51cc422ec7726f2
Dmytro <122775021+ShadowDrake21@users.noreply.github.com>
2023-05-06 11:34:12 +0300

    Delete CourseProject directory

--- CourseProject/src/transaction/IfromTo.java
+++
@@ -1,9 +0,0 @@
-package transaction;
-
-import java.awt.Component;
-
-public interface IfromTo {
-	public void onOut(Student st); // ������ �������� ����������
-	public void onIn(Student st); // ����� �������� ����������
-	public Component getComponent();
-}
